a8c6da2df73d4d671046c0b43b26ec5dd4bcfb74
NatNyt <70100268+NatNyt@users.noreply.github.com>
2023-05-26 16:51:04 +0700

    Fix  bug __dirname

--- src/util/configReader.ts
+++ src/util/configReader.ts
@@ -1,11 +1,15 @@
 import yaml from 'yaml';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 interface Config {
     token: string;
     applicationID: string
 }
+const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
 const config:Config = yaml.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'config.yml'), {encoding: 'utf8'}));
 
 export default config
